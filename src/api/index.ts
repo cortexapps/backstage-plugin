49e0cea4b55c03f5e74c76974586c28956635566
Nikhil Unni <nikhil.unni3000@gmail.com>
2021-08-25 13:41:55 -0700

    fix copyright notices

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 Cortex Applications Inc.
+ * Copyright 2021 Cortex Applications, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,5 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 export { CortexClient, cortexApiRef } from './CortexClient';
