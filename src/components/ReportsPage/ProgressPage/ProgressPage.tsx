2dd3f1c8db2505a47f31776c6ed1be88d97732ae
Nikhil Unni <nikhil.unni3000@gmail.com>
2021-10-05 16:04:49 -0700

    lint

--- src/components/ReportsPage/ProgressPage/ProgressPage.tsx
+++ src/components/ReportsPage/ProgressPage/ProgressPage.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import {
   Content,
   ContentHeader,
