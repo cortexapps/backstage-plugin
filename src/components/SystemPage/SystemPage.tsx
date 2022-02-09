a158add462a9c04b9d4b3b5bcb4e412cdb74de60
kgrining <kgrining@gmail.com>
2022-02-09 04:34:29 +0100

    CET-381 add aggregate scorecard scores to the system view (#33)

---
+++ src/components/SystemPage/SystemPage.tsx
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2021 Cortex Applications, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React, { useMemo } from 'react';
+import { Table, TableBody } from '@material-ui/core';
+import { Content, EmptyState, InfoCard } from '@backstage/core-components';
+import { useEntityFromUrl } from '@backstage/plugin-catalog-react';
+import { parseEntityName } from '@backstage/catalog-model';
+
+import { useCortexApi } from '../../utils/hooks';
+import { defaultSystemRefContext } from '../../utils/ComponentUtils';
+import { useDetailCardStyles } from '../../styles/styles';
+import { SystemPageRow } from './SystemPageRow';
+
+import { GroupByOption } from '../../api/types';
+
+export const SystemPage = () => {
+  const classes = useDetailCardStyles();
+  const { entity } = useEntityFromUrl();
+  const { value: serviceGroupScores } = useCortexApi(
+    api => api.getServiceScorecardScores(GroupByOption.SERVICE_GROUP),
+    [GroupByOption.SERVICE_GROUP],
+  );
+  const mySystemScores = useMemo(
+    () =>
+      serviceGroupScores?.filter(serviceGroupScore => {
+        const { name } = parseEntityName(
+          serviceGroupScore?.identifier ?? '',
+          defaultSystemRefContext,
+        );
+        return name === entity?.metadata.name;
+      })?.[0],
+    [serviceGroupScores, entity],
+  );
+  const myComponents = useMemo(
+    () =>
+      entity?.relations
+        ?.filter(({ type }) => type === 'hasPart')
+        .map(({ target }) => target.name),
+    [entity],
+  );
+  const { value: scorecardScores } = useCortexApi(
+    api =>
+      mySystemScores?.scores
+        ? Promise.all(
+            mySystemScores?.scores.map(score =>
+              api.getScorecardScores(score.scorecardId),
+            ),
+          )
+        : Promise.resolve([]),
+    [mySystemScores],
+  );
+  const data = useMemo(
+    () =>
+      mySystemScores?.scores.map((score, index) => ({
+        score,
+        scoresForScorecard:
+          scorecardScores?.[index]?.filter(({ componentRef }) =>
+            myComponents?.includes(componentRef),
+          ) ?? [],
+      })),
+    [mySystemScores, myComponents, scorecardScores],
+  );
+
+  return (
+    <Content>
+      <InfoCard title="System Scorecards" className={classes.root}>
+        {data?.length === 0 ? (
+          <EmptyState missing="data" title="No scorecards found." />
+        ) : (
+          <Table>
+            <TableBody>
+              {data?.map(_ => (
+                <SystemPageRow
+                  key={_.score.scorecardId}
+                  score={_.score}
+                  scoresForScorecard={_.scoresForScorecard}
+                />
+              ))}
+            </TableBody>
+          </Table>
+        )}
+      </InfoCard>
+    </Content>
+  );
+};
---
+++ src/components/SystemPage/SystemPageRowDetails.tsx
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2021 Cortex Applications, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import React from 'react';
+import { Box, makeStyles, Table, TableCell, TableRow } from '@material-ui/core';
+
+import { Gauge } from '../Gauge/Gauge';
+
+import { ScorecardServiceScore } from '../../api/types';
+import {DefaultEntityRefLink} from "../DefaultEntityLink";
+import {parseEntityName} from "@backstage/catalog-model";
+import {defaultComponentRefContext} from "../../utils/ComponentUtils";
+
+interface Props {
+  scoresForScorecard: ScorecardServiceScore[];
+}
+
+const useStyles = makeStyles({
+  root: {
+    '& > *': {
+      borderBottom: 'unset!important',
+    },
+    height: '15px',
+  },
+  progress: {
+    height: '32px',
+    marginRight: '16px',
+  },
+});
+
+export const SystemPageRowDetails: React.FC<Props> = ({
+  scoresForScorecard,
+}) => {
+  const classes = useStyles();
+
+  return (
+    <Table>
+      {scoresForScorecard.map(score => (
+        <TableRow className={classes.root} key={score.serviceId}>
+          <TableCell>
+            <Box
+              flexDirection="row"
+              display="flex"
+              justifyContent="flex-start"
+              alignItems="center"
+            >
+              <Box alignSelf="center" width={1 / 16}>
+                <Gauge
+                  value={score.scorePercentage}
+                  strokeWidth={8}
+                  trailWidth={8}
+                />
+              </Box>
+              <Box alignSelf="center" flex="1">
+                {score && (
+                  <DefaultEntityRefLink entityRef={parseEntityName(score.componentRef, defaultComponentRefContext)}/>
+                )}
+              </Box>
+            </Box>
+          </TableCell>
+        </TableRow>
+      ))}
+    </Table>
+  );
+};
