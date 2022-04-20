b712d56c5f3931e10fdc28e911700b47ee3cdd57
cristinabuenahora <cristinabuenahora@gmail.com>
2022-04-20 12:06:04 +0200

    remove unused import

--- src/components/FilterCard/Filters.tsx
+++ src/components/FilterCard/Filters.tsx
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import React, { useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import {
   Checkbox,
   Grid,
