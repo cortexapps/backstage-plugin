2a5d0aa3b9653131b1d811fb08fdeb3cfa0c353e
Nikhil Unni <nikhil.unni@cortex.io>
2023-08-23 11:39:24 -0400

    fix typo

--- src/api/CortexClient.ts
+++ src/api/CortexClient.ts
@@ -118,8 +118,8 @@ export class CortexClient implements CortexApi {
       await post(`/api/backstage/v2/entities/sync-chunked`, {
         entities: [],
         teamOverrides: {
-          teams: teamOverridesRelationshipsChunk,
-          relationships: []
+          teams: [],
+          relationships: teamOverridesRelationshipsChunk,
         }
       })
     }
