f972e518038700439e5cffaf584aa3074860b959
voytekgar <wojciech.garncarz@cortex.io>
2023-12-15 12:37:31 +0100

    CET-5850   [Backstage] Respects tenant end date

--- src/components/Entitlements/ExpirationUtils.ts
+++ src/components/Entitlements/ExpirationUtils.ts
@@ -1,14 +1,31 @@
+/*
+ * Copyright 2023 Cortex Applications, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 import {ContractType, ExpirationResponse} from "../../api/types";
 
-import { isNil } from 'lodash';
+import {isNil} from 'lodash';
 import moment from "moment/moment";
 
 export const shouldShowExpirationBanner = (
   expiration?: ExpirationResponse
 ) => {
+  console.debug('expiration ', expiration);
+
   // Always show banner for trial
   if (!isNil(expiration) && expiration.contractType === ContractType.Trial) {
-    return false;
+    return true;
   }
 
   const daysUntilExpiration =
@@ -16,11 +33,7 @@ export const shouldShowExpirationBanner = (
       ? moment(expiration?.expirationDate).diff(moment(), 'days')
       : undefined;
 
-  const ret =  !isNil(daysUntilExpiration) && daysUntilExpiration < 14;
-
-  console.debug('expiration ', expiration);
-
-  return ret;
+  return !isNil(daysUntilExpiration) && daysUntilExpiration < 14;
 };
 
 export const isBeforeShutdownDate = (
