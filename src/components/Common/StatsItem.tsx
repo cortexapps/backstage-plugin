b24140eb202451dbaf86e24d6491650fc41ec797
Igor Róg <igor.roog@gmail.com>
2024-02-07 21:37:48 +0100

    CET-6941 Fix StatsItem color (#153)

--- src/components/Common/StatsItem.tsx
+++ src/components/Common/StatsItem.tsx
@@ -17,7 +17,6 @@ import React, { useMemo } from 'react';
 import { percentify } from '../../utils/NumberUtils';
 import { ordinal } from '../../utils/strings';
 import { Box, Typography, alpha, withStyles } from '@material-ui/core';
-import { fallbackPalette } from '../../styles/styles';
 
 export type StatNumberType = 'NTH' | 'PERCENTAGE' | 'NONE';
 
@@ -28,11 +27,11 @@ export interface StatsItemProps {
   value: number;
 }
 
-export const CaptionTypography = withStyles({
+export const CaptionTypography = withStyles(theme => ({
   root: {
-    color: alpha(fallbackPalette.common.white, 0.7),
+    color: alpha(theme.palette.text.primary, 0.7),
   },
-})(Typography);
+}))(Typography);
 
 const StatsItem: React.FC<StatsItemProps> = ({
   caption,
