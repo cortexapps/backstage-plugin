152a631af80ea0fe724deef1dbf39cf0ee143ece
Igor Róg <igor.roog@gmail.com>
2024-04-09 22:11:42 +0200

    CET-8084 Backstage version update from 1.12.1 to 1.25.2 (#160)

--- scripts/run_branch.sh
+++ scripts/run_branch.sh
@@ -16,7 +16,7 @@ cd $plugin_dir
 plugin_version=$(cat package.json | python -c "import sys; import json; value = json.loads(sys.stdin.read()); print(value[\"version\"])")
 echo "Building and publishing backstage-plugin@${plugin_version}"
 
-if ! (nvm install 14.17.0 && yarn install && yarn tsc && yarn build && yalc publish --push) ; then
+if ! (nvm install 20.10.0 && yarn install && yarn tsc && yarn build && yalc publish --push) ; then
     echo 'Failed to build plugin.'
     exit 1
 fi
@@ -29,6 +29,6 @@ if [ ! -d "${backstage_dir}" ] ; then
     echo "export { cortexPlugin } from '@cortexapps/backstage-plugin';" > packages/app/src/plugins.ts
     patch -p1 < ../diff.patch
     yalc add "@cortexapps/backstage-plugin@${plugin_version}" --pure
-    nvm install 14.17.0
+    nvm install 20.10.0
     yarn install
 fi
