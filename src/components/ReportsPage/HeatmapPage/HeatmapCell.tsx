71b825c6a8bbf95d5b4aaf45c8f335d0c7a7d346
Pavel Žůrek <pavelzrk@gmail.com>
2024-04-12 09:55:11 +0200

    heatmap text readability

--- src/components/ReportsPage/HeatmapPage/HeatmapCell.tsx
+++ src/components/ReportsPage/HeatmapPage/HeatmapCell.tsx
@@ -22,35 +22,43 @@ const useStyles = makeStyles<BackstageTheme>(theme => ({
   root: {
     border: 'none',
     textAlign: 'center',
-    color: theme.palette.common.black,
     minWidth: '100px',
   },
   success1: {
     background: '#00ca9b',
+    color: theme.palette.common.black,
   },
   success2: {
     background: '#60e6c7',
+    color: theme.palette.common.black,
   },
   success3: {
     background: '#b0ecde',
+    color: theme.palette.common.black,
   },
   warning1: {
     background: '#ffecb3',
+    color: theme.palette.common.black,
   },
   warning2: {
     background: '#ffca28',
+    color: theme.palette.common.black,
   },
   warning3: {
     background: '#ffa001',
+    color: theme.palette.common.black,
   },
   danger1: {
     background: '#ffd6dd',
+    color: theme.palette.common.black,
   },
   danger2: {
     background: '#fe899e',
+    color: theme.palette.common.black,
   },
   danger3: {
     background: '#fd496a',
+    color: theme.palette.common.black,
   },
 }));
 
