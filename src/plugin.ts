c8f0f7f6542c89739bac706a4fba1f227c2c2b79
Alexandre Couedelo <a.couedelo@efg.gg>
2025-02-14 15:46:37 +0000

    feat: extract the CortexInsightsCard, so I can be added to any page

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,4 +1,19 @@
 /*
+ * Copyright 2025 Cortex Applications, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
  * Copyright 2024 Cortex Applications, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -199,6 +214,18 @@ export const CortexHomepage = cortexPlugin.provide(
     component: {
       lazy: () =>
         import('./components/Homepage/Homepage').then(m => m.Homepage),
+    },
+  }),
+);
+
+export const CortexInsightsCard = cortexPlugin.provide(
+  createComponentExtension({
+    name: 'CortexHomepage',
+    component: {
+      lazy: () =>
+        import('./components/Homepage/HomepageInsights').then(
+          m => m.HomepageInsights,
+        ),
     },
   }),
 );
